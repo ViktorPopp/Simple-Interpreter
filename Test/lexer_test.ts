cc5f4ed92b2a04d9d1d2dc771e0dcfa10edf24ac
Viktor Popp <viktorpopp.dev@gmail.com>
2024-12-28 13:49:35 +0100

    Added VSCode tasks and cleaned up tests

--- Test/lexer_test.ts
+++ Test/lexer_test.ts
@@ -1,7 +1,7 @@
 import { tokenize } from "../Source/lexer.ts";
 
-Deno.test("Lexer test", () => {
-    const source = "let x = (4 * 3)";
+Deno.test("Lexer test", async () => {
+    const source = await Deno.readTextFile("./Test/Sources/lexer_test.txt");;
     console.log(`Program to test: \"${source}\"`);
     for (const token of tokenize(source)) {
         console.log(token);
